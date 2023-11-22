c90d696f27e9e340ff418d9a37ecfa804cc35641
Manu <manurajbhar12@gmail.com>
2023-11-22 21:06:54 +0530

    Update index.tsx

--- pages/index.tsx
+++ pages/index.tsx
@@ -20,7 +20,7 @@ export default function Home() {
         <link rel="icon" href="/favicon.ico" />
       </Head>
       <main className={styles.main}>
-        <GreetingBox/>
+{/*         <GreetingBox/> */}
         <NavBar/>
         <Banner/>
         <div className={styles.info}>
